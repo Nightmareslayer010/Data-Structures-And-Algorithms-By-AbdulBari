9d31f0a3bf964684d574aa446c58bb717b89e240
Giriraj <girirajvyas010@gmail.com>
2026-05-18 07:34:18 +0530

    strings

---
+++ 05_strings/strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char *S = "voltra";
+    int i;
+    for (i = 0; S[i] != '\0'; i++)
+    {
+    }
+    cout << "length is" << i;
+
+    return 0;
+}
