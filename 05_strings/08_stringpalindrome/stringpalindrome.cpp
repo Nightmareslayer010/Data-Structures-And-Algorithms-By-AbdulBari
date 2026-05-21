1a7e3a730079dfb5fb942c2bded73d601767f98f
Giriraj <girirajvyas010@gmail.com>
2026-05-21 09:00:45 +0530

    string palindrome

--- 05_strings/08_stringpalindrome/stringpalindrome.cpp
+++ 05_strings/08_stringpalindrome/stringpalindrome.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void swap(char *x, char *y)
-{
-    if (x == y)
-        return;
-    *x = *x ^ *y;
-    *y = *x ^ *y;
-    *x = *x ^ *y;
-}
 int main()
 {
     char A[] = "madam";
-    int i = 0;
+    bool flag = true;
     int j = 0;
     for (; A[j] != '\0'; j++)
     {
     }
     j = j - 1;
-    while (i < j)
+    for (int i = 0; i < j; i++, j--)
     {
-        swap(&A[i], &A[j]);
         if (A[i] != A[j])
         {
-            cout << "not palindrome" << endl;
+            cout << "not palindrome";
+            flag = false;
             break;
         }
-        i++;
-        j--;
     }
-    if (A[i] == A[j])
+    if (flag)
     {
-        cout << "palindrome";
+        cout << "paldindrome";
     }
-
     return 0;
 }
