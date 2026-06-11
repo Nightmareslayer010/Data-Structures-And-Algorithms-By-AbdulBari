d5ffe0887cb99d62162164697634d1f65fc6deb1
Giriraj <girirajvyas010@gmail.com>
2026-06-11 08:57:02 +0530

    matrix patterns

--- 06_matrices/00_patterns/03_lowertriangularpattern/lowertriangularpattern.cpp
+++ 06_matrices/00_patterns/03_lowertriangularpattern/lowertriangularpattern.cpp
@@ -3,25 +3,20 @@ using namespace std;
 
 int main()
 {
-    int v1 = 2;
-    int v2 = 2;
-
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 5; j++)
         {
-            if (j >= v1 && j <= v2)
+            if (j <= i)
             {
                 cout << 1;
             }
             else
             {
-                cout << " ";
+                cout << 0;
             }
         }
         cout << endl;
-        v1--;
-        v2++;
     }
 
     return 0;
